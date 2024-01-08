35c0ef6bdc24912e3751e2863aff1a4284e56836
devs-shakil <mdshakil102036@gmail.com>
2024-01-08 21:44:08 +0600

    image with text section completed

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 import ImageWithText from "./components/ImageWithText/ImageWIthText";
-
+import image from './../public/images/header-right.png'
 
 
 export default function Home() {
   return (
   <main>
      <ImageWithText
-      title={"We Build Your Brand Online"}
+      title={"Perfect Clipping Path & Photoshop Services"}
       subtitle={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus, risus sit amet auctor sodales, justo erat tempor eros"}
+      image={image}
+      imageFirst = {false}
+      buttonOne={"Get started"}
+      buttonTwo={"See Portfolio"}
      />
   </main>
     
