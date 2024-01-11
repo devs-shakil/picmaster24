bb04dcf5c5b40d1ab76799311ca1e7ee27465cef
devs-shakil <mdshakil102036@gmail.com>
2024-01-11 09:01:01 +0600

    working on footer

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { FaFacebook, FaInstagramSquare, FaTwitter ,FaWhatsapp  } from "react-icons/fa";
-import {navigationLinks, socialIcon } from "./../../data/navigation"
+import {navigationLinks, socialIcon, servicesMenu, helpfullLink, contactUs } from "./../../data/navigation"
 
 
 export default function Footer() {
@@ -29,7 +29,7 @@ export default function Footer() {
                         href={link.href}
                         rel="noreferrer"
                         target="_blank"
-                        className="text-teal-700 transition hover:text-teal-700/75"
+                        className="color-white transition hover:text-teal-700/75 bg-secondary "
                         >
                        {link.icon}
                     </Link>
@@ -38,14 +38,14 @@ export default function Footer() {
         </ul>
       </div>
 
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4 lg:col-span-2">
+      <div className="grid grid-cols-2 gap-8 sm:grid-cols-2 md:grid-cols-4 lg:col-span-2">
         <div className="text-center sm:text-left">
-          <p className="text-lg font-medium color-heading">Our Services</p>
+          <p className="text-lg font-medium color-heading">Quick Link</p>
 
           <ul className="mt-8 space-y-4 text-sm">
           { navigationLinks.map((link, index) =>(
             <li key={index}>
-                <Link className="color-primary transition hover:text-gray-700/75 " href={link.href}>
+                <Link className="color-primary transition hover:color-heading" href={link.href}>
                     {link.label}
                 </Link>
             </li>
@@ -54,140 +54,54 @@ export default function Footer() {
         </div>
 
         <div className="text-center sm:text-left">
-          <p className="text-lg font-medium text-gray-900">Our Services</p>
+          <p className="text-lg font-medium color-heading">Our Services</p>
 
           <ul className="mt-8 space-y-4 text-sm">
-            <li>
-              <a className="text-gray-700 transition hover:text-gray-700/75" href="/">
-                Web Development
-              </a>
-            </li>
-
-            <li>
-              <a className="text-gray-700 transition hover:text-gray-700/75" href="/"> Web Design </a>
-            </li>
-
-            <li>
-              <a className="text-gray-700 transition hover:text-gray-700/75" href="/"> Marketing </a>
-            </li>
-
-            <li>
-              <a className="text-gray-700 transition hover:text-gray-700/75" href="/"> Google Ads </a>
-            </li>
+            {servicesMenu.map((link, index)=>(
+                <li key={index}>
+                    <Link 
+                        className="color-white transition hover:color-heading"
+                        href={link.href}>
+                            {link.label}
+                    </Link>
+                </li>
+            ))}
           </ul>
         </div>
 
         <div className="text-center sm:text-left">
-          <p className="text-lg font-medium text-gray-900">Helpful Links</p>
+          <p className="text-lg font-medium color-heading">Helpful Links</p>
 
           <ul className="mt-8 space-y-4 text-sm">
-            <li>
-              <a className="text-gray-700 transition hover:text-gray-700/75" href="/"> FAQs </a>
-            </li>
-
-            <li>
-              <a className="text-gray-700 transition hover:text-gray-700/75" href="/"> Support </a>
-            </li>
-
-            <li>
-              <a
-                className="group flex justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
-                href="/"
-              >
-                <span className="text-gray-700 transition group-hover:text-gray-700/75">
-                  Live Chat
-                </span>
-
-                <span className="relative flex h-2 w-2">
-                  <span
-                    className="absolute inline-flex h-full w-full animate-ping rounded-full bg-teal-400 opacity-75"
-                  ></span>
-                  <span className="relative inline-flex h-2 w-2 rounded-full bg-teal-500"></span>
-                </span>
-              </a>
-            </li>
+            {helpfullLink.map((link, index)=>(
+                <li key={index}>
+                    <Link 
+                        className="color-white transition "
+                        href={link.href}>
+                        {link.label}
+                    </Link>
+                </li>
+            ))}
+           
           </ul>
         </div>
 
         <div className="text-center sm:text-left">
-          <p className="text-lg font-medium text-gray-900">Contact Us</p>
+          <p className="text-lg font-medium color-heading">Contact Us</p>
 
           <ul className="mt-8 space-y-4 text-sm">
-            <li>
-              <a
-                className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
-                href="/"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 shrink-0 text-gray-900"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-
-                <span className="flex-1 text-gray-700">john@doe.com</span>
-              </a>
-            </li>
-
-            <li>
-              <a
-                className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
-                href="/"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 shrink-0 text-gray-900"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                  />
-                </svg>
-
-                <span className="flex-1 text-gray-700">0123456789</span>
-              </a>
-            </li>
-
-            <li
-              className="flex items-start justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 shrink-0 text-gray-900"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-
-              <address className="-mt-0.5 flex-1 not-italic text-gray-700">
-                213 Lane, London, United Kingdom
-              </address>
-            </li>
+            { contactUs.map((link, index)=>(
+                <li key={index}>
+                    <Link
+                        className="flex items-center justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end "
+                        href={link.href}
+                    >
+                        <span className="color-white">{link.icon}</span>
+                        <span className="flex-1 color-white">{link.label}</span>
+                    </Link>
+                </li>
+            ))}
+           
           </ul>
         </div>
       </div>
@@ -197,25 +111,9 @@ export default function Footer() {
       <div className="text-center sm:flex sm:justify-between sm:text-left">
         <p className="text-sm text-gray-500">
           <span className="block sm:inline">All rights reserved.</span>
-
-          <a
-            className="inline-block text-teal-600 underline transition hover:text-teal-600/75"
-            href="/"
-          >
-            Terms & Conditions
-          </a>
-
-          <span>&middot;</span>
-
-          <a
-            className="inline-block text-teal-600 underline transition hover:text-teal-600/75"
-            href="/"
-          >
-            Privacy Policy
-          </a>
         </p>
 
-        <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">&copy; 2022 Company Name</p>
+        <p className="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">&copy; 2024 <Link href="/">Picmaster24</Link></p>
       </div>
     </div>
   </div>
--- app/data/navigation.js
+++ app/data/navigation.js
@@ -1,12 +1,16 @@
-import { FaFacebook, FaInstagramSquare, FaTwitter ,FaWhatsapp  } from "react-icons/fa";
+import { FaFacebook, FaInstagramSquare, FaTwitter ,FaWhatsapp,FaPhone   } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
 
 export const navigationLinks = [
-    { label: "Company History", href: "/" },
-    { label: "Meet the Team", href: "/" },
-    { label: "Employee Handbook", href: "/" },
-    { label: "Careers", href: "/" },
+    { label: "Home", href: "/" },
+    { label: "About Us", href: "/about" },
+    { label: "Pricing", href: "/pricing" },
+    { label: "Porfolio", href: "/portfolio" },
+    { label: "Contact", href: "/contact" }
   ];
 
+  
 // social media
   export const socialIcon = [
     { icon: <FaFacebook/> , href: "/" },
@@ -14,3 +18,27 @@ export const navigationLinks = [
     { icon: <FaTwitter/>, href: "/" },
     { icon: <FaWhatsapp/>, href: "/" },
   ];
+
+  // services 
+  export const servicesMenu = [
+    { label: "Clipping path", href: "/" },
+    { label: "Image Masking Service", href: "/" },
+    { label: "Photo Retouching Service", href: "/" },
+    { label: "Shadow & mirror effects", href: "/" },
+    { label: "Ghost Mannequin Effect", href: "/" },
+  ];
+
+// helpfull link
+export const helpfullLink = [
+    { label: "FAQs", href: "/" },
+    { label: "Support", href: "/" },
+    { label: "Live Chat", href: "/" },
+  ];
+  
+//contact us
+export const contactUs = [
+    { label: "mail@picmaster24.com", href: "/", icon:  <IoMail />},
+    { label: "8801780783657", href: "/" , icon: <FaPhone/> },
+    { label: "Mirpur 10, Dhaka , Bangladesh", href: "/", icon: <FaLocationDot /> },
+  ];
+  
