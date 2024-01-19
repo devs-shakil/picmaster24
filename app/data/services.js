db0d82ad80192246be93ddd409eed2b643329735
devs-shakil <mdshakil102036@gmail.com>
2024-01-20 00:14:57 +0600

    working on services section

---
+++ app/data/services.js
@@ -0,0 +1,18 @@
+export const servicesItems = [
+    { 
+      image:'/images/services1.jpg', 
+      title: "Clipping Path",
+      subtitle: "In digital image process it generally becomes necessary to separate sure objects or elements of the..." ,
+    },
+    { 
+      image:'/images/services2.jpg',
+      title: "Color Correction & Editing",
+      subtitle: "Color adjustment service is occupied for changing a image color or matching the image color with..." ,
+    },
+    { 
+      image:'/images/services3.jpg',
+      title: "Ghost Mannequin Effect",
+      subtitle: "For various catalogs and e-commerce sites, mannequins are used instead of human figure to create 3D..." ,
+    },
+  ];
+  
