835ef8ac5c523dd790a975970f6c46e56d54f472
devs-shakil <mdshakil102036@gmail.com>
2024-01-30 22:47:52 +0600

    whatsapp setup

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import './globals.css'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import CallToAction from './components/CallToAction/CallToAction'
+import Link from 'next/link'
+import { FaWhatsappSquare } from "react-icons/fa";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +25,13 @@ export default function RootLayout({ children }) {
         <footer className='max-w-full overflow-x-hidden'>
             <Footer/>
         </footer>
-      
+        <Link
+          className="whatsappLink fixed right-2 lg:right-16 bottom-14 lg:bottom-8 p-1 rounded-sm  inline-flex items-center justify-center"
+          href="https://wa.me/+8801642080280"
+          target='_blank'
+        >
+          <FaWhatsappSquare />
+        </Link>
       </body>
     </html>
   )
